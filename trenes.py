49dae02b999b291ae67f9981fad4680311a89af4
raul <192793@iberopuebla.mx>
2022-10-23 00:27:30 -0500

    Todavía falta corregir errores y añadir prioridad

--- trenes.py
+++ trenes.py
@@ -1,16 +1,44 @@
+import threading
+trenA=[0,3,4]
+trenB=[1,3,4]
+trenC=[2,4]
+#tramos=[trenA,trenB,trenC,[],[]]
 
-trenA=[2,4]
-trenB=[2,4]
-trenC=[4]
-tramos=[trenA,trenB,trenC,[],[]]
+class monitor:
+    def __init__(self):
+        self.tramos=[[],[],[],[],[]]
+        self.lock= threading.Lock()
+        self.cv = threading.Condition()
 
+    def mover(self,tren,id):
+        target=tren[0]
+        self.tramos[target].append(tren)
+        #print("se añadio")
+        while self.tramos[target][0]!=tren:
+            #print("esperando")
+            with self.cv:
+                self.cv.wait()
+        tren.pop(0)
+        self.tramos[target].pop(0)
+        print("El tren "+str(id)+" se movió al tramo " + str(target))
 
-def mover(tren,tramos):
-    target=tren[0]
-    tren.pop(0)
-    tramos[target]=tren
+def tren(mo:monitor, tren, id):
+    while(tren != []):
+        mo.mover(tren,id)
+    print("El tren " +str(id) +" llegó a su destino")
 
+trenes =[trenA, trenB, trenC]
+t=[]
+mo = monitor()
 
-mover(trenA, tramos)
-print(tramos)
-print(trenA)
+t0 =threading.Thread(target=tren, args=(mo, trenes[0],0))
+t1 =threading.Thread(target=tren, args=(mo, trenes[1],1))
+t2 =threading.Thread(target=tren, args=(mo, trenes[2],2))
+
+t0.start()
+t1.start()
+t2.start()
+
+t0.join()
+t1.join()
+t2.join()
